4ed4d8bf41de62b7a8c47804268eb8990a8a5d1a
sxntiagoad <salvarezd3@eafit.edu.co>
2025-01-28 09:52:14 -0500

    phone number changed

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -84,7 +84,7 @@ function LandingPage() {
 
       {/* WhatsApp Flotante */}
       <a 
-        href="https://wa.me/+573135210285" 
+        href="https://wa.me/+573108307428" 
         target="_blank" 
         rel="noopener noreferrer" 
         className="fixed bottom-6 right-6 bg-green-500 text-white p-3 rounded-full shadow-lg hover:bg-green-600 transition-colors z-50"
